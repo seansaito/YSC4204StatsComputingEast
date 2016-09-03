c2fb3629ddf220c05eff65dde73ad40e0806ddc0
Han Chong <chongwoonhan@gmail.com>
2016-09-03 18:52:28 +0800

    Added 4 Files
    NOTE: the 2 .png files need to be in the same folder as the .tex file so that LateX will be able to generate the pdf.
    
    hw02_latex.tex : the main latex file we will use to generate our PDF
    
    rayleigh.R : the R code I used for Question 4
    
    rayleigh_0_5.png : a histogram for Question 4
    
    rayleigh_2.png : a histogram for Question 4

---
+++ hw02/rayleigh.R
@@ -0,0 +1,31 @@
+## Define function to generate Rayleigh random variables ##
+rray <- function(n,sigma){
+  u <- runif(n)
+  output <- sqrt(-2*sigma^2*log(u))
+  return(output)
+}
+
+## Set seed to ensure consistency before generating numbers and plotting ##
+set.seed(0)
+
+########## simga = 0.5 ##########
+x1 <- rray(10000,0.5)
+hist(x1,prob=TRUE,
+     main="Generated rayleigh random variables with sigma=0.5",
+     xlab="x") # plot the histogram
+
+## Superimpose a density line ##
+xlines1 <- seq(0,round(max(x1)),0.01)
+ylines1 <- (xlines1/0.5^2)*exp(-xlines1^2/(2*(0.5^2)))
+lines(xlines1,ylines1)
+
+########## sigma = 2 ##########
+x2 <- rray(10000,2)
+hist(x2,prob=TRUE,
+     main="Generated rayleigh random variables with sigma=2",
+     xlab="x") # plot the histogram
+
+## Superimpose a density line ##
+xlines2 <- seq(0,round(max(x2)),0.01)
+ylines2 <- (xlines2/2^2)*exp(-xlines2^2/(2*(2^2)))
+lines(xlines2,ylines2)
