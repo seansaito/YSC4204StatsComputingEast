a63daa9ebbb0df4c7e95aa59c44ec3d609fa095e
Han Chong <chongwoonhan@gmail.com>
2016-09-03 17:18:30 +0800

    Update hw02 to include problem 4

--- hw02/hw02.R
+++ hw02/hw02.R
@@ -58,3 +58,33 @@ pythag2 <- function(a, b) {
     stop("ERROR: pythag2() needs numeric input.")
   return(sqrt(a^2 + b^2))
 }
+
+# Problem 4 - Rayleigh Distribution
+
+## Define function to generate Rayleigh random variables ##
+rray <- function(n,sigma){
+  u <- runif(n)
+  output <- sqrt(-2*sigma^2*log(u))
+  return(output)
+}
+
+## Set seed to ensure consistency before generating numbers and plotting ##
+set.seed(0)
+
+########## simga = 0.5 ##########
+x1 <- rray(10000,0.5)
+hist(x1,prob=TRUE,main="Generated rayleigh random variables with sigma=0.5") # plot the histogram
+
+## Superimpose a density line ##
+xlines1 <- seq(0,round(max(x1)),0.01)
+ylines1 <- (xlines1/0.5^2)*exp(-xlines1^2/(2*(0.5^2)))
+lines(xlines1,ylines1)
+
+########## sigma = 2 ##########
+x2 <- rray(10000,2)
+hist(x2,prob=TRUE,main="Generated rayleigh random variables with sigma=2") # plot the histogram
+
+## Superimpose a density line ##
+xlines2 <- seq(0,round(max(x2)),0.01)
+ylines2 <- (xlines2/2^2)*exp(-xlines2^2/(2*(2^2)))
+lines(xlines2,ylines2)
