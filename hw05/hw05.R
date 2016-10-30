e3c62ab1f0492408e4107c8302b3a7a62838bd11
Han Chong <chongwoonhan@gmail.com>
2016-10-30 17:42:14 +0800

    added function g for Question 2

--- hw05/hw05.R
+++ hw05/hw05.R
@@ -102,3 +102,7 @@ cat("\nResult after 2nd iteration is ", iter4)
 
 #### Question 2 ####
 x <- c(-7.99,24.11,3.41,7.01,22.28,0.54,-1.97,15.97,11.53,237.63)
+
+g <- function(gam){
+  return(10/gam + sum(2*gam/(x^2+gam^2)))
+}
